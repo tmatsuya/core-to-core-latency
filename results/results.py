fd24c189257abf8dbf260e6c0ac1b740d425b296
Takeshi Matsuya <macchan@sfc.wide.ad.jp>
2025-10-10 16:59:20 +0900

    results.py: add python script for drawing heatmap.

---
+++ results/results.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+
+def load_data(filename):
+    m = np.array(pd.read_csv(filename, header=None))
+    return np.tril(m) + np.tril(m).transpose()
+
+def show_heapmap(m, title=None, subtitle=None, vmin=None, vmax=None, yticks=True, figsize=None, pngname="output.png"):
+    vmin = np.nanmin(m) if vmin is None else vmin
+    vmax = np.nanmax(m) if vmax is None else vmax
+    black_at = (vmin+3*vmax)/4
+    subtitle = "Core-to-core latency" if subtitle is None else subtitle
+    
+    isnan = np.isnan(m)
+
+    plt.rcParams['xtick.bottom'] = plt.rcParams['xtick.labelbottom'] = False
+    plt.rcParams['xtick.top'] = plt.rcParams['xtick.labeltop'] = True
+
+    figsize = np.array(m.shape)*0.3 + np.array([6,1]) if figsize is None else figsize
+    fig, ax = plt.subplots(figsize=figsize, dpi=130)
+    
+    fig.patch.set_facecolor('w')
+    
+    plt.imshow(np.full_like(m, 0.7), vmin=0, vmax=1, cmap = 'gray') # for the alpha value
+    plt.imshow(m, cmap = plt.cm.get_cmap('viridis'), vmin=vmin, vmax=vmax)
+    
+    fontsize = 9 if vmax >= 100 else 10
+
+    for (i,j) in np.ndindex(m.shape):
+        t = "" if isnan[i,j] else f"{m[i,j]:.1f}" if vmax < 10.0 else f"{m[i,j]:.0f}"
+        c = "w" if m[i,j] < black_at else "k"
+        plt.text(j, i, t, ha="center", va="center", color=c, fontsize=fontsize)
+        
+    plt.xticks(np.arange(m.shape[1]), labels=[f"{i+1}" for i in range(m.shape[1])], fontsize=9)
+    if yticks:
+        plt.yticks(np.arange(m.shape[0]), labels=[f"CPU {i+1}" for i in range(m.shape[0])], fontsize=9)
+    else:
+        plt.yticks([])
+
+    plt.tight_layout()
+    plt.title(f"{title}\n" +
+              f"{subtitle}\n" +
+              f"Min={vmin:0.1f}ns Median={np.nanmedian(m):0.1f}ns Max={vmax:0.1f}ns",
+              fontsize=11, linespacing=1.5)
+
+    fig.tight_layout()
+    fig.savefig(pngname, transparent=False, facecolor='white')
+
+
+
+cpu = "Intel(R) Xeon Phi(TM) CPU 7210 @ 1.30GHz (64 cores, Knights Landing)"
+fname = "Intel(R) Xeon Phi(TM) CPU 7210 @ 1.30GHz.csv"
+pngname = "Intel(R) Xeon Phi(TM) CPU 7210 @ 1.30GHz.png"
+m = load_data(fname)
+show_heapmap(m, title=cpu, pngname=pngname)
